2b07d5638c644750fa8997ae40bf717a6d548a61
Eli Citron <ebcitron@gmail.com>
2019-05-09 11:45:10 -0500

    Added PORT env variable

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(process.env.PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${process.env.port} ***\n`);
 });
